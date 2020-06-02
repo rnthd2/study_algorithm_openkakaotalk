4deec4012a64206777a51fdf899637ad271c19aa
구송이 <rnthd2@tmon.co.kr>
2020-06-02 21:39:39 +0900

    [4*4 미로찾기 문제] 재귀로 마무리

--- src/data/structure/graph/FourByFourMazeRecursion.java
+++ src/data/structure/graph/FourByFourMazeRecursion.java
@@ -39,46 +39,51 @@ public class FourByFourMazeRecursion {
 	static final int EXIT = 2;
 	static final int VISITED = 2;
 
-	static Integer[][] maze = new Integer[ROWS][COLS];
+	static Integer[][] question = new Integer[ROWS][COLS];
+	static Integer[][] result= new Integer[ROWS][COLS];
 
 	public static void main(String[] args) {
 
-		String[] strList = { "1 1 0 1", "0 1 1 1", "0 0 0 1", "0 1 0 1" };
-//		String[] strList = { "1 0 0 0", "1 1 0 0", "0 1 0 0", "0 1 1 1" };
+//		String[] strList = { "1 1 0 1", "0 1 1 1", "0 0 0 1", "0 1 0 1" };
+		String[] strList = { "1 0 0 0", "1 1 0 0", "0 1 0 0", "0 1 1 1" };
 
 		int row = 0;
 		do {
 			String str = strList[row].replaceAll(" ", "");
 			for (int col = 0; col < COLS; col++) {
-				maze[row][col] = Integer.valueOf(String.valueOf(str.charAt(col)));
+				question[row][col] = Integer.valueOf(String.valueOf(str.charAt(col)));
 			}
 			row++;
-		} while (maze[ROWS-1][COLS-1]==null);
+		} while (question[ROWS-1][COLS-1]==null);
 
 		findPath(0, 0);
+
+		for (int i = 0; i < question.length; i++) {
+			for (int j = 0; j < question.length; j++) {
+				System.out.print(((question[i][j] != NOTPATH)? PATH : NOTPATH) + " ");
+			}
+			System.out.println();
+		}
 	}
 
 	// https://www.youtube.com/watch?v=m6lXDsx7oCk를 참고해서 이래저래 고민해보았지만, 아래 코드로 하면 동떨어진 애들까지 커버할 수 없어서
-	// 이 코드안에서는 해결이 안되고 maze clone 후 짜넣어야된다
+	// 이 코드안에서는 해결이 안되고 question clone 후 짜넣어야된다
 	static boolean findPath(int row, int col) {
 		if (row < 0 || col < 0 || row >= ROWS || col >= COLS) {
 			return false; //is wall
-		} else if (maze[row][col] != PATH) {
+		} else if (question[row][col] != PATH) {
 			return false; // not path(visited, wall, block)
 		} else if (row == ROWS - 1 && col == COLS - 1) {
-			maze[row][col] = EXIT;
+			question[row][col] = EXIT;
 			return true;
 		} else {
-			maze[row][col] = VISITED;
+			question[row][col] = VISITED;
 			if(findPath(row-1, col) || findPath(row,col+1) || findPath(row+1,col) || findPath(row,col-1)){
 				return true;
 			}
-			maze[row][col] = NOTPATH;
+			question[row][col] = NOTPATH;
 			return false;
 		}
-
-
-
 	}
 
 }
