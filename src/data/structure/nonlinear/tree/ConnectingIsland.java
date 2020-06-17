f18f9db3903a1c18e6feaee4e94c012fff626109
구송이 <rnthd2@tmon.co.kr>
2020-06-17 15:26:02 +0900

    [최소신장트리 섬 연결하기] 구현 완료

---
+++ src/data/structure/nonlinear/tree/ConnectingIsland.java
@@ -0,0 +1,63 @@
+package data.structure.nonlinear.tree;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ConnectingIsland {
+	static List<ArrayList> sets = new ArrayList();
+
+	static int solution(int n, int[][] costs) {
+		List<Integer> answerList = new ArrayList();
+
+		//선택정렬을 이용한 가중치 오름차순으로 정렬
+		for (int i = 0; i < costs.length-1; i++) {
+			for (int j = i+1; j < costs.length; j++) {
+				if(costs[i][2] > costs[j][2])
+					costs = swap(costs, i, j);
+			}
+		}
+
+		//각각의 섬을 하나의 집합으로 세팅
+		for (int i = 0; i < n; i++) {
+			sets.add(new ArrayList(Arrays.asList(i)));
+		}
+
+		//오름차순으로 가중치 최소 경로 선택
+		for (int i = 0; i < costs.length; i++) {
+			if (findSet(costs[i][0]) != findSet(costs[i][1])) {
+				answerList.add(costs[i][2]);
+
+				//집합 세팅
+				for (ArrayList set : sets) {
+					if(set.contains(costs[i][1])) {
+						sets.remove(set);
+						break;
+					}
+				}
+				sets.get(costs[i][0]).add(costs[i][1]);
+			}
+			if(answerList.size() == n-1) break;
+		}
+		return answerList.stream().mapToInt(e -> e).sum();
+	}
+
+	public static void main(String[] args) {
+		int n = 4;
+		int[][] costs = {{0,1,1},{0,2,2},{1,2,5},{1,3,1},{2,3,8}};
+		int result = solution(n, costs);
+		System.out.println(result);
+	}
+
+	static int[][] swap(int[][] costs, int i, int j){
+		int[] temp = costs[j];
+		costs[j] = costs[i];
+		costs[i] = temp;
+		return costs;
+	}
+
+	static List<ArrayList> findSet(int x){
+		return sets.stream().filter(set -> set.contains(x)).collect(Collectors.toList());
+	}
+}
