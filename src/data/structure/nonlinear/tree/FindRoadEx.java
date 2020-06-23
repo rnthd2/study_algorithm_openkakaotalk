b7fad67d32cbbc28fdcc60954abe08338bacf6a1
구송이 <rnthd2@tmon.co.kr>
2020-06-23 09:05:34 +0900

    [카카오 2019 이진트리 문제] 구현 중
    부모 노드 세팅 필요

--- src/data/structure/nonlinear/tree/FindRoadEx.java
+++ src/data/structure/nonlinear/tree/FindRoadEx.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
-class FindRoadNode implements Comparable<FindRoadNode>{
+class FindRoadNodeEx implements Comparable<FindRoadNodeEx>{
 	Object element;
 	int x;
 	int y;
-	FindRoadNode leftChild;
-	FindRoadNode rightChild;
-	FindRoadNode parent;
+	FindRoadNodeEx leftChild;
+	FindRoadNodeEx rightChild;
+	FindRoadNodeEx parent;
 
-	public FindRoadNode(Object element, int x, int y) {
+	public FindRoadNodeEx(Object element, int x, int y) {
 		this.element = element;
 		this.x = x;
 		this.y = y;
@@ -27,20 +27,20 @@ class FindRoadNode implements Comparable<FindRoadNode>{
 	}
 
 	//첫번째 방법
-	@Override public int compareTo(FindRoadNode o) {
+	@Override public int compareTo(FindRoadNodeEx o) {
 		return this.getY() > o.getY()? 1 : -1;
 	}
 }
 
 //두번째 방법
-class FindRoadComparator implements Comparator<FindRoadNode> {
+class FindRoadComparatorEx implements Comparator<FindRoadNodeEx> {
 	@Override
-	public int compare(FindRoadNode o1, FindRoadNode o2) {
+	public int compare(FindRoadNodeEx o1, FindRoadNodeEx o2) {
 		return o2.getY() > o1.getY() ? 1 : -1;
 	}
 }
 
-public class FindRoad {
+public class FindRoadEx {
 
 	public static void main(String[] args) {
 		int[][] nodeinfo = { { 5, 3 }, { 11, 5 }, { 13, 3 }, { 3, 5 }, { 6, 1 }, { 1, 3 }, { 8, 6 }, { 7, 2 }, { 2, 2 } };
@@ -49,18 +49,18 @@ public class FindRoad {
 
 	public static int[][] solution(int[][] nodeinfo) {
 		int[][] answer = {};
-		ArrayList<FindRoadNode> list = new ArrayList<>();
+		ArrayList<FindRoadNodeEx> list = new ArrayList<>();
 
 		// 노드 세팅
 		for (int i = 0; i < nodeinfo.length; i++) {
-			list.add(new FindRoadNode(i, nodeinfo[i][0], nodeinfo[i][1]));
+			list.add(new FindRoadNodeEx(i, nodeinfo[i][0], nodeinfo[i][1]));
 		}
 
 		// y를 기준으로 내림차순
 //		Collections.sort(list, new FindRoadComparator());
 		//세번째 방법
-//		Collections.sort(list, new Comparator<FindRoadNode>() {
-//			@Override public int compare(FindRoadNode o1, FindRoadNode o2) {
+//		Collections.sort(list, new Comparator<FindRoadNodeEx>() {
+//			@Override public int compare(FindRoadNodeEx o1, FindRoadNodeEx o2) {
 //				return o2.getY() > o1.getY() ? 1 : -1;
 //			}
 //		});
