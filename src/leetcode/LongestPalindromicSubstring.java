b896d6c7a4b447db6101221ed30a3d46f012fafa
rnthd2 <rnthd2@naver.com>
2020-08-04 12:59:50 +0900

    [리트코드 2주차] 회문 문자열 중 가장 긴 문자열 가져오기
    Runtime: 583 ms, faster than 5.68% of Java online submissions for Longest Palindromic Substring.
    Memory Usage: 37.3 MB, less than 98.66% of Java online submissions for Longest Palindromic Substring.

--- src/leetcode/LongestPalindromicSubstring.java
+++ src/leetcode/LongestPalindromicSubstring.java
@@ -1,38 +1,54 @@
 package leetcode;
 
-import java.util.HashMap;
-
 public class LongestPalindromicSubstring {
 
 	public static void main(String[] args) {
 		LongestPalindromicSubstring longestPalindromicSubstring = new LongestPalindromicSubstring();
 		String result1 = longestPalindromicSubstring.longestPalindrome("babad");
-		String result2 = longestPalindromicSubstring.longestPalindrome("a");
-		String result3 = longestPalindromicSubstring.longestPalindrome("");
-		String result4 = longestPalindromicSubstring.longestPalindrome("ccc");
 		System.out.println(result1);
+		String result2 = longestPalindromicSubstring.longestPalindrome("a");
 		System.out.println(result2);
+		String result3 = longestPalindromicSubstring.longestPalindrome("");
 		System.out.println(result3);
+		String result4 = longestPalindromicSubstring.longestPalindrome("abb");
 		System.out.println(result4);
+				String result5 = longestPalindromicSubstring.longestPalindrome("ab");
+				System.out.println(result5);
+				String result6 = longestPalindromicSubstring.longestPalindrome("CCC");
+				System.out.println(result6);
+				String result7 = longestPalindromicSubstring.longestPalindrome("babadada");
+				System.out.println(result7);
+
 	}
 
 	public String longestPalindrome(String s) {
-		if(s.length()<1) return "";
-		HashMap<Character, Integer> map = new HashMap();
-		int start=0, end=0;
-		
-		for (int i = 0; i < s.length(); i++) {
-			char c = s.charAt(i);
-			if(map.containsKey(c)){
-				if(i-map.get(c) > end-start){
-					start = map.get(c);
-					end = i;
+		int length=0;
+		int begin=0;
+		for (int start = 0; start < s.length(); start++) {
+			for (int end = s.length()-1; end >=start; end--) {
+				boolean isPalindrome = true;
+				int left = start, right = end;
+				//회문 확인
+				while (left < right) {
+					if (s.charAt(left) != s.charAt(right)) {
+						isPalindrome = false;
+						break;
+					} else {
+						++left;
+						--right;
+					}
 				}
-			} else {
-				map.put(c, i);
+				if (isPalindrome && length <= end-start) {
+					length = end-start+1;
+					begin = start;
+				}
+				//max값 즉시 리턴
+				if(length == s.length()){
+					return s.substring(start, s.length());
+				}
 			}
 		}
-
-		return s.substring(start, end+1);
+		return s.substring(begin, begin+length);
 	}
+
 }
