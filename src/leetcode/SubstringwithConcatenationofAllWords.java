a3aacbb54caab130857850a6c5135a47f8af8dee
rnthd2 <rnthd2@naver.com>
2020-09-29 13:18:03 +0900

    [리트코드 9주차] 연속된 단어의 substring start index 구하기 완료

--- src/leetcode/SubstringwithConcatenationofAllWords.java
+++ src/leetcode/SubstringwithConcatenationofAllWords.java
@@ -9,37 +9,48 @@ public class SubstringwithConcatenationofAllWords {
 //        s = "barfoothefoobarman", words = ["foo","bar"]
 //        s = "wordgoodgoodgoodbestword", words = ["word","good","best","word"]
 //        s = "barfoofoobarthefoobarman", words = ["bar","foo","the"]
-        System.out.println(findSubstring("barfoothefoobarman", new String[]{"foo", "bar"}));
+//        System.out.println(findSubstring("barfoothefoobarman", new String[]{"foo", "bar"}));
 //        System.out.println(findSubstring("wordgoodgoodgoodbestword", new String[]{"word", "good", "best", "word"}));
-        System.out.println(findSubstring("barfoofoobarthefoobarman",new String[]{"foo", "bar", "the"}));
+//        System.out.println(findSubstring("barfoofoobarthefoobarman",new String[]{"foo", "bar", "the"}));
+        System.out.println(findSubstring("lingmindraboofooowingdingbarrwingmonkeypoundcake"
+                ,new String[]{"fooo", "barr", "wing", "ding", "wing"}));
 //        System.out.println(new ArrayList<Integer>(Arrays.asList(6,9)));
+    }
+
+    static boolean test(int length, String s, ArrayList<String> words) {
+        int left = 0, right = left + length;
+
+        //s를 words.length로 나눈다
+        String str = s.substring(left, right);
+
+        while (!str.isEmpty() && !words.isEmpty()
+                && left < right && right <= s.length()) {
+            str = s.substring(left, right);
+            if (words.contains(str)) words.remove(str);
+            else
+                return false;
+            left = right;
+            right = left + length;
+
+        }
+        return true;
     }
 
     public static List<Integer> findSubstring(String s, String[] words) {
         int length = words[0].length();
         List<Integer> list = new ArrayList<>();
-        List<String> temp = new ArrayList<>(Arrays.asList(words));
 
-        int left = 0
-                , i = -1  //all starting indices of substring
-                , right = left + length;
+        int left = 0, right = length*words.length;
 
-        while (left < right && right <= s.length()){
+        while (left < right && right <= s.length()) {
             String str = s.substring(left, right);
-
-            if (temp.contains(str)) {
-                if (temp.size() == words.length) i = left;
-                temp.remove(str);
-                if (temp.isEmpty()){
-                    list.add(i);
-                    i = -1;
-                }
-            } else {
-                temp = new ArrayList<>(Arrays.asList(words));
-                right = i + length;
+            
+            if(test(length, str, new ArrayList<>(Arrays.asList(words)))){
+                list.add(left);
             }
-            left = right;
-            right = left + length;
+
+            left ++;
+            right = length*words.length + left;
 
         }
 
