99c9d66bfddd6d9dc9db84335aa1326bf514dfe4
rnthd2 <rnthd2@naver.com>
2021-04-14 14:55:22 +0900

    [프로그래머스 1주차] 해시 완주 완료

--- src/programmers/coding/test/kit/hash/Solution3.java
+++ src/programmers/coding/test/kit/hash/Solution3.java
@@ -10,7 +10,8 @@ import java.util.HashMap;
  */
 public class Solution3 {
     public static void main(String[] args) {
-        String [][] clothes = new String [][] {{"crowmask", "face"}, {"bluesunglasses", "face"}, {"smoky_makeup", "face"}};
+//        String [][] clothes = new String [][] {{"crowmask", "face"}, {"bluesunglasses", "face"}, {"smoky_makeup", "face"}};
+        String [][] clothes = new String [][] {{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}};
         System.out.println(solution(clothes));
     }
 
@@ -19,34 +20,20 @@ public class Solution3 {
         HashMap<String, Integer> map = new HashMap();
         for (int i = 0; i < clothes.length; i++) {
             String s = clothes[i][1];
-            map.put(s, map.getOrDefault(s, 0) + 1);
+            //입지 않는 경우 추가를 위하여 1을 기본값으로 한다
+            //의상 종류 개수 추가를 위하여 1을 더한다
+            map.put(s, map.getOrDefault(s, 1) + 1);
         }
 
-        int result = 0;
+        //하루에 최소 한개 이상의 의상을 입는다
+        int result = 1;
 
+        for (Integer count : map.values()) {
+            result *= count;
+        }
 
-
-
-
-
-
-
-        
-        /*for (int r = map.size(); r > 0; r--) {
-            double d = factorial(map.size())/(r*(map.size()-r));
-            int count = Integer.parseInt(String.valueOf(d));
-            while(count>0){
-                Iterator<Integer> iterator = map.values().iterator();
-                while(iterator.hasNext()){
-                    iterator.next();
-                }
-                count--;
-            }
-
-        }*/
-
-
-        return 0;
+        //모두 입지 않는 경우의 수를 뺀다
+        return result-1;
     }
     /**
      * 팩토리얼
